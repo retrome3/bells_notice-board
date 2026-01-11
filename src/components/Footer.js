0c17849bae70d8ef2ccb0c6673e4d93319886191
retrome3 <sebastianfox2350@gmail.com>
2026-01-11 15:24:35 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ import '../styles/App.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>© 2025 Bells University of Technology · ICT 235 Frontend Development Project</p>
-        <p className="subtext">Digital Notice Board | Team Submission</p>
-      </div>
+      <p>© 2025 Bells University of Technology - ICT 235 Frontend Development Project</p>
+      <p>Digital Notice Board | Team Submission</p>
     </footer>
   );
 };
